ab18a6f8be9c5318d04a76d81aa1e0d9d5bc943c
Daniel Alexssander <danielrossinatti15@gmail.com>
2024-04-03 19:16:45 -0300

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Solve CORS
 // { credentials: true, origin: "http://localhost:3000" }
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({ credentials: true, origin: "https://www.youtube.com" }));
 
 // Upload directory
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
